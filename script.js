9a9c8bb5ebc98ba1d967e07b490579e04d91ebe2
Kayalvaer <62592167+Kayalvaer@users.noreply.github.com>
2020-05-25 00:17:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log ("3*5");
+// question 1
+
+// Answer for question 1 
+var firstVar
+
+// question 2
+
+// Answer for question 2 
+var name=("Kay")
+
+// question 3
+
+// Answer for question 3
+var number;
+number=Value
+console.log(number);
+
+// question 4
+
+// Answer for question 4
+var vision=20/5;
+console.log(division);
+
+// question 5
+
+// Answer for question 5
+var frog;
+console.log(typeof "frog");
+
+
+// question 6
+
+// Answer for question 6
+var orderHasShipped = true;
+
+
+// question 7
+
+// Answer for question 7
+var orderHasShipped = ["true"];
+if (orderHasShipped ===true) {
+    let Value="true";
+    console.log("True")
+} else {
+    let Value="false";
+    console.log("False")
+    
+}
+
+// question 8
+
+// Answer for question 8
+var counter = ["1,2,3,4,5,6,7,8,9"];
+for (var i=0; i<=9; i++) {
+    console.log(counter ["i"]);
+    
+}
